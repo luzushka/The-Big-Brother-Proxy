dfe11a883d2b36e189f366f54b368d52a4a6e528
luzushka <igonnabanu@gmail.com>
2015-02-08 12:10:43 +0200

    301+302 improvements

--- serverUtils.py
+++ serverUtils.py
@@ -3,14 +3,14 @@ import re
 import os
 import sys
 import httplib
-
+import urllib2
 ROOT = "."
 err404="<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>404 Not Found</h1>\n</body></html>\n"
 err403="<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>403 Forbidden</h1>\n</body></html>\n"
 err302="<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n<html><head>\n<title>302 Found</title>\n</head><body>\n<h1>302 Found. File has been temporarly moved to /file2.txt</h1>\n</body></html>\n"
 def isvalidreq (string): #checks if the request is a valid http get request
     listt=string.split(' ')
-    return (listt[0]=='GET' and listt[2][:8]!='HTTP/1.1') or  (listt[0]=='GET' and listt[2][:8]!='HTTP/1.0')
+    return (listt[0]=='GET')#and listt[2][:8]!='HTTP/1.1') and listt[2][:8]!='HTTP/1.0')
 def gethost (string): #gets the host from the get string
     i=string.find('Host')
     return string[i+6:string.find("\r\n",i+5)]
@@ -46,9 +46,8 @@ def one_client_request(cs, addr):
     #Dealing with one client's request
 ##    while True:
     try:
-        print "alala="+str(type(cs))
-        print "alalaaaaa"+str(cs)
-        print dir(cs)
+        
+        
         txt=cs.recv(1024) #getting requested by our beloved client
         print txt
         print "isvalid" + str(isvalidreq(txt))
@@ -59,9 +58,36 @@ def one_client_request(cs, addr):
             try:
                 csa.request("GET", getget(txt),headers={})
                 r1 = csa.getresponse()
-
+                print "nana",r1.status
                 senttxt=r1.read()
-                cs.send(senttxt)
+                if r1.status==302 or r1.status==301:
+                    print "respp"
+                    resp=urllib2.urlopen("http://"+gethost(txt))
+                    print resp.geturl()
+                    loc=resp.geturl()[7:]
+##                    print "MOVED ERR" , r1.reason
+##                    loc=r1.getheader('Location')
+##                    print "Original Loc", loc
+##                    print "loc",loc[7:]
+                    csa.close()
+                    print "csa closed after another loc", loc[:loc.find('/')]
+                    csa = httplib.HTTPConnection(loc[:loc.find('/')])
+                    print "able to connect to new csa"
+                    print "before get succeeded",loc[loc.find('/'):]
+                    csa.request("GET",loc[loc.find('/')+1:] ,headers={})
+                    print "hirfun"
+                    r1 = csa.getresponse()
+                    print r1.status
+                    #print "nanana",r1.read()
+                    print "lalalalala"
+                    senttxta=r1.read()
+                    cs.send(senttxta)
+                    print "text was sent1"
+                    csa.close()
+                    cs.close()
+                else:
+                    cs.send(senttxt)
+                    print "text was sent2"
             except:
                 cs.send("404 not found")
                 cs.close()
